92eb484e241848e7582099583b686b89815df85e
Mmishaaa <kkochin2528@gmail.com>
2024-03-04 23:12:31 +0300

    feat: add userInput typr

--- src/routes/graphql/GraphQLSchema/user/schema.ts
+++ src/routes/graphql/GraphQLSchema/user/schema.ts
@@ -73,3 +73,19 @@ export const GraphQlUserType = new GraphQLObjectType({
     },
   }),
 });
+
+export const CreateUserInput = new GraphQLInputObjectType({
+  name: 'CreateUserInput',
+  fields: () => ({
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    balance: { type: new GraphQLNonNull(GraphQLFloat) },
+  }),
+});
+
+export const ChangeUserInput = new GraphQLInputObjectType({
+  name: 'ChangeUserInput',
+  fields: () => ({
+    name: { type: GraphQLString },
+    balance: { type: GraphQLFloat },
+  }),
+});
